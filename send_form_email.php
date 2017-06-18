053bfde7eeeea04f6a9720640102dda06d265960
MARTIN-PC\Martin <martin.k6re@gmail.com>
2017-06-18 10:45:25 +0300

    dsa

--- send_form_email.php
+++ send_form_email.php
@@ -4,14 +4,10 @@ if(isset($_POST['submit'])){
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
     $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     echo "success";
     }
